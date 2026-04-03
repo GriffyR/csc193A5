0d6d5aa988111d6a8eca05351dbf7693baa3285e
Ryan <135178659+GriffyR@users.noreply.github.com>
2026-04-03 16:44:55 -0700

    Complete.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,10 +1,53 @@
-/*
-Create an alert box. 
-This is just a test to make sure you're running JS code.
 
-*/
+const b1 = document.createElement('b1');
+let fancy = document.getElementsByID();
 
-const button = document.createElement('b1');
+function makeBigger()
+{
+    alert("Hello, world!"); // Test for the JavaScript
+    let textArea = document.getElementById("textInput");
+    textArea.style.fontSize = "24pt";
+}
+
+function makeFancy()
+{
+    let textArea = document.getElementById("textInput");
+    let fancyRadio = document.getElementById("fancy");
+
+    if (fancyRadio.checked)
+    {
+        alert("FancyShmancy selected!");
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+    }
+    else
+    {
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.textDecoration = "none";
+    }
+}
 
 
-alert("Hello, world!");
+function mooText()
+{   
+    let textArea = document.getElementById("textInput");
+    textArea.value = textArea.value.toUpperCase();
+
+    let  sentences = textArea.value.split(".");
+
+    for (let i = 0; i < sentences.length; i++) 
+    {
+        let sentence = sentences[i].trim();
+
+        if (sentence.length > 0) 
+        {
+            let words = sentence.split(" ");
+            words[words.length - 1] = words[words.length - 1] + "-Moo";
+            sentences[i] = words.join(" ");
+        }
+
+        textArea.value = sentences.join(".");
+    }
+}
